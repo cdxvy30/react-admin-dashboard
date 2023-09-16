9ecf9b1649db32ce68a76fea0f1ba06f0fe265f0
Cody Chen <cdxvy30@gmail.com>
2023-09-16 22:29:46 +0800

    branch: first commit for scon-web

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,6 +1,6 @@
 import Datatable from "../../components/datatable/Datatable";
 import Navbar from "../../components/navbar/Navbar";
-import Sidebar from "../../components/sidebar/Sidebar";
+import { Sidebar } from "../../components/sidebar/Sidebar";
 import "./list.scss";
 
 const List = () => {
